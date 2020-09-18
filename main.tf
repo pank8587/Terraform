dab0749f49371a5a376663a0cd508f4dedf184fb
pank8587 <dubeyp36@gmail.com>
2020-09-18 13:33:50 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  credentials = file("terraform-keyfile.json")
+  project     = "{{ox-sigmoid-prod}}"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+      ssd_dsik = 100
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "sigmoid-openx"
+    access_config {
+    }
+  }
+}
+
